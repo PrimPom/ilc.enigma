ef6510a645f646311d6a06268a70017e2813416e
am483811 <am483811@chichester.esirem-ad.ad.u-bourgogne.fr>
2020-01-13 15:17:42 +0100

    premiers tests pour la classe Rotor

--- src/test/java/enigma/RotorTest.java
+++ src/test/java/enigma/RotorTest.java
@@ -7,8 +7,22 @@ import org.junit.Test;
 public class RotorTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void forwad() {
+            Rotor r = Rotor.rotorFactory(
+				"A B C D E F G H I J K L M N O P Q R S T U V W X Y Z", "E");
+            r.setPosition(0);
+            char actual = r.toLetter(r.convertForward(2));
+            assertEquals('C', actual);
 	}
-
+        
+        @Test
+	public void backward() {
+            Rotor r = Rotor.rotorFactory(
+				"A B C D E F G H I J K L M N O P Q R S T U V W X Y Z", "E");
+            r.setPosition(2);
+            char actual = r.toLetter(r.convertForward(0));
+            assertEquals('A', actual);
+	}
+        
+        
 }
